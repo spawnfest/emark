7743f583839e247dc796a0629b5e67bfbacf41aa
Serge Zirukin <ftrvxmtrx@gmail.com>
2012-07-07 15:29:21 +0200

    compile with -DBENCHMARK; set codepath; set cwd

--- src/emark_plugin.erl
+++ src/emark_plugin.erl
@@ -12,6 +12,7 @@ emark(Config, _AppFile) ->
   ok = filelib:ensure_dir(ebin_dir() ++ "/lol"),
 
   %% setup code paths
+  CodePath = code:get_path(),
   true = code:add_patha(emark_dir()),
   true = code:add_pathz(ebin_dir()),
 
@@ -24,15 +25,25 @@ emark(Config, _AppFile) ->
                         [],
                         SrcDirs),
 
-  rebar_erlc_compiler:doterl_compile(emark_config(Config), ?EMARK_DIR, SrcErls).
+  %% compile with -DBENCHMARK
+  rebar_erlc_compiler:doterl_compile(emark_config(Config), ?EMARK_DIR, SrcErls),
+
+  Beams = rebar_utils:beams(?EMARK_DIR),
+  Modules = [ rebar_utils:beam_to_mod(?EMARK_DIR, B) || B <- Beams ],
+
+  perform_benchmark(Config, Modules),
+
+  %% restore code path
+  true = code:set_path(CodePath),
+  ok.
 
 clean(_Config, _File) ->
   rebar_file_utils:rm_rf(?EMARK_DIR).
 
 %===============================================================================
 
-emark_dir() ->
-  filename:join(rebar_utils:get_cwd(), ?EMARK_DIR).
+benchmark(_Modules, _EmarkOpts) ->
+  ok.
 
 ebin_dir() ->
   filename:join(rebar_utils:get_cwd(), "ebin").
@@ -40,11 +51,27 @@ ebin_dir() ->
 emark_config(Config0) ->
   ErlOpts = rebar_config:get_list(Config0, erl_opts, []),
   EmarkOpts = rebar_config:get_list(Config0, emark_compile_opts, []),
-  Opts0 = [ { d, 'TEST' } ] ++ ErlOpts ++ EmarkOpts,
+  Opts0 = [ { d, 'BENCHMARK' } ] ++ ErlOpts ++ EmarkOpts,
   Opts1 = [ Opt || Opt <- Opts0, Opt =/= no_debug_info ],
   Config1 = rebar_config:set(Config0, erl_opts, Opts1),
   FirstErls = rebar_config:get_list(Config1, emark_first_files, []),
   rebar_config:set(Config1, erl_first_files, FirstErls).
+
+emark_dir() ->
+  filename:join(rebar_utils:get_cwd(), ?EMARK_DIR).
+
+get_emark_opts(Config) ->
+  rebar_config:get_list(Config, emark_opts, []).
+
+perform_benchmark(Config, Modules) ->
+  Cwd = rebar_utils:get_cwd(),
+  ok = file:set_cwd(?EMARK_DIR),
+
+  EmarkOpts = get_emark_opts(Config),
+  EmarkResult = benchmark(Modules, EmarkOpts),
+
+  ok = file:set_cwd(Cwd),
+  EmarkResult.
 
 %%% Local Variables:
 %%% erlang-indent-level: 2
