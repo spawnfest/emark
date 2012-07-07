16cde75552a07c6415bd892f4618ef9faf5a211f
Serge Zirukin <ftrvxmtrx@gmail.com>
2012-07-07 14:33:53 +0200

    compile files using rebar

--- src/emark_plugin.erl
+++ src/emark_plugin.erl
@@ -3,8 +3,44 @@
 -export([ emark/2
         ]).
 
-emark(_Config, _AppFile) ->
-  rebar_log:log(warn, "OMG LOL WTF~n", []).
+-define(EMARK_DIR, ".emark").
+
+emark(Config, _AppFile) ->
+  %% ensure ?EMARK_DIR and ebin dirs exist
+  ok = filelib:ensure_dir(emark_dir() ++ "/lol"),
+  ok = filelib:ensure_dir(ebin_dir() ++ "/lol"),
+
+  %% setup code paths
+  true = code:add_patha(emark_dir()),
+  true = code:add_pathz(ebin_dir()),
+
+  %% copy source files to ?EMARK_DIR
+  SrcDirs = rebar_config:get_list(Config, src_dirs, [ "src" ]),
+  SrcErls = lists:foldl(fun(Dir, Accu) ->
+                            Files = rebar_utils:find_files(Dir, ".*\\.erl\$"),
+                            Accu ++ Files
+                        end,
+                        [],
+                        SrcDirs),
+
+  rebar_erlc_compiler:doterl_compile(emark_config(Config), ?EMARK_DIR, SrcErls).
+
+%===============================================================================
+
+emark_dir() ->
+  filename:join(rebar_utils:get_cwd(), ?EMARK_DIR).
+
+ebin_dir() ->
+  filename:join(rebar_utils:get_cwd(), "ebin").
+
+emark_config(Config0) ->
+  ErlOpts = rebar_config:get_list(Config0, erl_opts, []),
+  EmarkOpts = rebar_config:get_list(Config0, emark_compile_opts, []),
+  Opts0 = [ { d, 'TEST' } ] ++ ErlOpts ++ EmarkOpts,
+  Opts1 = [ Opt || Opt <- Opts0, Opt =/= no_debug_info ],
+  Config1 = rebar_config:set(Config0, erl_opts, Opts1),
+  FirstErls = rebar_config:get_list(Config1, emark_first_files, []),
+  rebar_config:set(Config1, erl_first_files, FirstErls).
 
 %%% Local Variables:
 %%% erlang-indent-level: 2
