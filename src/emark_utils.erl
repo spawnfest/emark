11e0f8027004e5ecad6f907c1b760fbcb1c00639
Serge Zirukin <ftrvxmtrx@gmail.com>
2012-07-08 12:27:19 +0200

    utils: export _benchmark funcs if suffix is defined

--- src/emark_utils.erl
+++ src/emark_utils.erl
@@ -1,14 +1,25 @@
 -module(emark_utils).
 
 -export([ exports_of_forms/1
+        , exports_of_forms/2
         , fun_suffix/1
         ]).
 
 -include("emark_internal.hrl").
 
 exports_of_forms(Forms) ->
+  exports_of_forms(no_autoexport).
+
+exports_of_forms(Forms, Suffix) ->
   lists:foldl(fun({ attribute, _, export, ExList }, Accu) ->
                   sets:union(sets:from_list(ExList), Accu);
+                 ({ function, _, Name, 0, _ }, Accu) when is_list(Suffix) ->
+                  case lists:suffix(Name, Suffix) of
+                    true ->
+                      sets:add_element({ Name, 0 }, Accu);
+                    false ->
+                      Accu
+                  end;
                  (_, Accu) ->
                   Accu
               end,
