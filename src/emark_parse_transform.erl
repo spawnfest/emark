8396733563537d6da5f1d3d043a99fc2f48724fc
Serge Zirukin <ftrvxmtrx@gmail.com>
2012-07-08 13:15:00 +0200

    parse_transform: fixes

--- src/emark_parse_transform.erl
+++ src/emark_parse_transform.erl
@@ -16,15 +16,21 @@ transform([ Form | Forms ], Exports, Suffix) ->
 transform([], _Exports, _Suffix) ->
   [].
 
-module_decl({ attribute, _, module, { Module, _ } } = M, Forms, Exports, Suffix) ->
+module_decl({ attribute, _, module, { Module, _ } } = M,
+            Forms,
+            Exports,
+            Suffix) ->
   module_autoexport(Module, M, Forms, Exports, Suffix);
-module_decl({ attribute, _, module, Module } = M, Forms, Exports, Suffix) ->
+module_decl({ attribute, _, module, Module } = M,
+            Forms,
+            Exports,
+            Suffix) ->
   module_autoexport(Module, M, Forms, Exports, Suffix).
 
-funcs_cons([ Func | Funcs ]) ->
+funcs_cons([ { Func, 0 } | Funcs ]) ->
   { cons, 0
   , { 'fun', 0
-    , { function, list_to_atom(Func), 0 }
+    , { function, Func, 0 }
     }
   , funcs_cons(Funcs)
   };
@@ -34,10 +40,12 @@ funcs_cons([]) ->
 module_autoexport(_Module, M, Forms, Exports, Suffix) ->
   ExportsList = sets:to_list(Exports),
 
-  Funcs = lists:filter(fun(Name) ->
-                           lists:suffix(Name, Suffix)
+  Funcs = lists:filter(fun({ Name, _ }) ->
+                           lists:suffix(Suffix, atom_to_list(Name))
                        end,
                        ExportsList),
+
+  rebar_log:log(debug, "Benchmark funcs: ~p~n", [ Funcs ]),
 
   Benchmark = { function, 0, benchmark, 0
               , [ { clause, 0, [], []
@@ -48,7 +56,14 @@ module_autoexport(_Module, M, Forms, Exports, Suffix) ->
 
   Ex = { attribute, 0, export, [ { benchmark, 0 } | ExportsList ] },
 
-  [ M, Ex, Benchmark | Forms ].
+  [ M, Ex, Benchmark | remove_exports(Forms) ].
+
+remove_exports([ { attribute, _, export, _ } | Forms ]) ->
+  remove_exports(Forms);
+remove_exports([ Form | Forms ]) ->
+  [ Form | Forms ];
+remove_exports([]) ->
+  [].
 
 %%% Local Variables:
 %%% erlang-indent-level: 2
