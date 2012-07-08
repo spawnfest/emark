8630dda968f5fcbe1afbec7a40eed1fb79abd1c0
Serge Zirukin <ftrvxmtrx@gmail.com>
2012-07-08 13:14:15 +0200

    fix default suffix

--- src/emark_internal.hrl
+++ src/emark_internal.hrl
@@ -2,6 +2,6 @@
 -define(EMARK_INTERNAL_HRL, awww_yeah).
 
 %% suffix which a func need to have in order to be benchmarked
--define(DEFAULT_FUN_SUFFIX, benchmark).
+-define(DEFAULT_FUN_SUFFIX, "_benchmark").
 
 -endif. % !EMARK_INTERNAL_HRL
