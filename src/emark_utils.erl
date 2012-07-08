cc335013e3401290da2bf7b103bebb87c18dfc18
Serge Zirukin <ftrvxmtrx@gmail.com>
2012-07-09 01:52:11 +0200

    docs

--- src/emark_utils.erl
+++ src/emark_utils.erl
@@ -1,3 +1,5 @@
+%% @doc Misc utils.
+
 -module(emark_utils).
 
 -export([ ceil/1
@@ -8,6 +10,7 @@
 
 -include("emark_internal.hrl").
 
+%% @doc Round a number to a greater one, looking a lot nicer.
 ceil(X) ->
   Floor = floor_10(X),
 
@@ -22,9 +25,11 @@ ceil(X) ->
       10 * Floor
   end.
 
+%% @doc Exports without those with "_benchmark" suffix.
 exports_of_forms(Forms) ->
   exports_of_forms(Forms, no_autoexport).
 
+%% @doc Exports, including those with "_benchmark" suffix.
 exports_of_forms(Forms, Suffix) ->
   F = fun({ attribute, _, export, ExList }, Accu) ->
           sets:union(sets:from_list(ExList), Accu);
@@ -44,8 +49,10 @@ exports_of_forms(Forms, Suffix) ->
 
   lists:foldl(F, sets:new(), Forms).
 
+%% @doc Round a number to a lower one.
 floor_10(X) ->
   trunc(math:pow(10, trunc(math:log10(X)))).
 
+%% @doc Get function suffix ("_benchmark") from emark options.
 fun_suffix(Options) ->
   proplists:get_value(emark_fun_suffix, Options, ?DEFAULT_FUN_SUFFIX).
